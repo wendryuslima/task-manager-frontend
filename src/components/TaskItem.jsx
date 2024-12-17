11bd7157efa3e09f17d5ebdbe2861d72660e1837
wendryuslima <141366401+wendryuslima@users.noreply.github.com>
2024-12-17 13:49:47 -0300

    feat: add function to delete a task

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,9 +1,18 @@
 import "../components/styles/TaskItem.scss";
 import { MdDelete } from "react-icons/md";
 import "./styles/Task.scss";
+import axios from "axios";
 
-const TaskItem = ({ task }) => {
-  return ( 
+const TaskItem = ({ task, fetchTasks }) => {
+  const handleDeleteCLick = async () => {
+    try {
+      await axios.delete(`http://localhost:8000/tasks/${task._id}`);
+      fetchTasks();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
     <>
       <div className="task-item-container">
         <div className="task-description">
@@ -23,7 +32,7 @@ const TaskItem = ({ task }) => {
         </div>
 
         <div className="delete">
-          <MdDelete size={18} color="#f97474" />
+          <MdDelete onClick={handleDeleteCLick} size={18} color="#f97474" />
         </div>
       </div>
     </>
